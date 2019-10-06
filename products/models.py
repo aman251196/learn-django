38de8465f0c77a32ea8f11972b6d1442463c4b94
oneEyedAman <aman.srivastava@ezedox.com>
2019-10-06 16:32:10 +0530

    Products changes: id to UUID change and meta data

--- products/models.py
+++ products/models.py
@@ -1,12 +1,26 @@
+import uuid
+
 from django.db import models
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-# Create your models here.
+    class Meta:
+        abstract = True
 
-class Product(models.Model):
+class Product(BaseModel):
     title       = models.CharField(max_length = 120, null=False, blank=False)
     description = models.TextField()
     price       = models.DecimalField(null=False, blank=False, max_digits=1000, decimal_places=2)
     summary     = models.TextField(default="This stuff is really cool")
 
-    
+    @property
+    def representation(self):
+        return "Name: {}".format(self.title)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.representation
      
